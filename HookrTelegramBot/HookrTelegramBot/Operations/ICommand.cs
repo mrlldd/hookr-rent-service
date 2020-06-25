543bdaf750676bafeef0b35fa4527c3b96b47817
Ivan Yeremenko <ivan.yeremenko@km-ware.us>
2020-06-25 18:53:13 +0300

    Initial create.

---
+++ HookrTelegramBot/HookrTelegramBot/Operations/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace HookrTelegramBot.Operations
+{
+    public interface ICommand
+    {
+        string Name { get; set; }
+        Task ExecuteAsync();
+    }
+}
