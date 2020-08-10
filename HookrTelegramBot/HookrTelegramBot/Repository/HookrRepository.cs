702faf05955755ba4f1de0dc3c11a50d8ede75f1
mrlldd <ivan.yeremenko@km-ware.us>
2020-08-10 17:50:29 +0300

    Implemented caching translations getter in hookr repository service.

--- HookrTelegramBot/HookrTelegramBot/Repository/HookrRepository.cs
+++ HookrTelegramBot/HookrTelegramBot/Repository/HookrRepository.cs
@@ -2,17 +2,25 @@
 using System.Threading;
 using System.Threading.Tasks;
 using HookrTelegramBot.Repository.Context;
+using HookrTelegramBot.Repository.Context.Entities.Translations;
 using HookrTelegramBot.Utilities.Resiliency;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace HookrTelegramBot.Repository
 {
     public class HookrRepository : IHookrRepository
     {
         private readonly IPolicySet policySet;
+        private readonly IMemoryCache memoryCache;
         public HookrContext Context { get; }
-        public HookrRepository(HookrContext context, IPolicySet policySet)
+
+        public HookrRepository(HookrContext context,
+            IPolicySet policySet,
+            IMemoryCache memoryCache)
         {
             this.policySet = policySet;
+            this.memoryCache = memoryCache;
             Context = context;
         }
 
@@ -22,5 +30,30 @@ namespace HookrTelegramBot.Repository
         public Task WriteAsync(Func<HookrContext, CancellationToken, Task> functor)
             => policySet.WritePolicy.ExecuteAsync(() => functor(Context, default));
 
+        public async Task<string> GetTranslationAsync(LanguageCodes languageCode,
+            TranslationKeys translationKey,
+            bool omitCache = false)
+        {
+            const string keyFormat = "t_l{0}_k{1}";
+            const double timeoutDays = 1;
+            var key = string.Format(keyFormat, languageCode, translationKey);
+            if (!omitCache
+                && memoryCache.TryGetValue<string>(key,
+                    out var result))
+            {
+                return result;
+            }
+
+            var translation = await policySet.ReadPolicy
+                .ExecuteAsync(() => Context.Translations
+                    .FirstOrDefaultAsync(x
+                        => x.Language == languageCode
+                           && x.Key == translationKey));
+            memoryCache.Set(key, translation.Value, new MemoryCacheEntryOptions
+            {
+                SlidingExpiration = TimeSpan.FromDays(timeoutDays)
+            });
+            return translation.Value;
+        }
     }
 }
--- HookrTelegramBot/HookrTelegramBot/Repository/IHookrRepository.cs
+++ HookrTelegramBot/HookrTelegramBot/Repository/IHookrRepository.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using HookrTelegramBot.Repository.Context;
+using HookrTelegramBot.Repository.Context.Entities.Translations;
 
 namespace HookrTelegramBot.Repository
 {
@@ -10,5 +11,9 @@ namespace HookrTelegramBot.Repository
         HookrContext Context { get; }
         Task<TResult> ReadAsync<TResult>(Func<HookrContext, CancellationToken, Task<TResult>> functor);
         Task WriteAsync(Func<HookrContext, CancellationToken, Task> functor);
+
+        Task<string> GetTranslationAsync(LanguageCodes languageCode,
+            TranslationKeys translationKey,
+            bool omitCache = false);
     }
 }
