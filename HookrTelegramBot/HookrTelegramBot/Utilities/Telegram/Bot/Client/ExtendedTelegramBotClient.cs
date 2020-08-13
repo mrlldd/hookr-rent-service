59b5b60e8079294a4a9f84754f882b904c3dd14f
mrlldd <ivan.yeremenko@km-ware.us>
2020-08-13 18:05:39 +0300

    Implemented preserved characters filtering.

--- HookrTelegramBot/HookrTelegramBot/Utilities/Telegram/Bot/Client/ExtendedTelegramBotClient.cs
+++ HookrTelegramBot/HookrTelegramBot/Utilities/Telegram/Bot/Client/ExtendedTelegramBotClient.cs
@@ -1,11 +1,17 @@
-﻿using HookrTelegramBot.Utilities.Telegram.Bot.Client.CurrentUser;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using HookrTelegramBot.Utilities.Extensions;
+using HookrTelegramBot.Utilities.Telegram.Bot.Client.CurrentUser;
 using HookrTelegramBot.Utilities.Telegram.Bot.Provider;
-using HookrTelegramBot.Utilities.Telegram.Selectors.UpdateMessage;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace HookrTelegramBot.Utilities.Telegram.Bot.Client
 {
     public class ExtendedTelegramBotClient : DecoratedTelegramBotClientBase, IExtendedTelegramBotClient
     {
+
         public ExtendedTelegramBotClient(
             IUserContextProvider userContextProvider,
             ITelegramBotProvider provider,
@@ -16,5 +22,30 @@ namespace HookrTelegramBot.Utilities.Telegram.Bot.Client
         }
 
         public ICurrentTelegramUserClient WithCurrentUser { get; }
+
+        public override Task<Message> SendTextMessageAsync(ChatId chatId,
+            string text,
+            ParseMode parseMode = ParseMode.Default,
+            bool disableWebPagePreview = false,
+            bool disableNotification = false,
+            int replyToMessageId = 0,
+            IReplyMarkup replyMarkup = null,
+            CancellationToken cancellationToken = default)
+        {
+            var content = text;
+            if (parseMode == ParseMode.MarkdownV2)
+            {
+                content = content.FilterPreservedCharacters();
+            }
+            return base
+                .SendTextMessageAsync(chatId,
+                    content,
+                    parseMode,
+                    disableWebPagePreview,
+                    disableNotification,
+                    replyToMessageId,
+                    replyMarkup,
+                    cancellationToken);
+        }
     }
 }
