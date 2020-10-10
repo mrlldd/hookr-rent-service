6880bad96dff111d4b63c0564468f564c2fbb184
mrlldd <akside2934@gmail.com>
2020-10-10 16:35:42 +0300

    Added Apps and Database folder

--- Hookr/Web/Hookr.Web.Backend/Operations/Queries/Auth/ConfirmAuthQueryHandler.cs
+++ Hookr/Web/Hookr.Web.Backend/Operations/Queries/Auth/ConfirmAuthQueryHandler.cs
@@ -18,26 +18,21 @@ namespace Hookr.Web.Backend.Operations.Queries.Auth
         }
 
         public override Task<bool> ExecuteQueryAsync(ConfirmAuthQuery query)
-            => DelegateFactory(query)
-                .Map(Task.Run);
-
-        private Func<bool> DelegateFactory(ConfirmAuthQuery query)
-            =>
-                () =>
-                {
-                    using var sha256 = SHA256.Create();
-                    var secretKey = sha256
-                        .ComputeHash(telegramConfig.Token.Utf8Bytes()); //todo pass bot token here from config
-                    using var hmacsha256 = new HMACSHA256(secretKey);
-                    var dataCheckStringHash = hmacsha256
-                        .ComputeHash(query.Key.Utf8Bytes());
-                    return new StringBuilder(dataCheckStringHash.Length *
-                                             2) // as actually unit length will increase by two
-                        .SideEffect(builder => dataCheckStringHash
-                            .ForEach(b => builder.AppendFormat("{0:x2}", b))
-                        )
-                        .ToString()
-                        .Equals(query.Hash);
-                };
+        {
+            using var sha256 = SHA256.Create();
+            var secretKey = sha256
+                .ComputeHash(telegramConfig.Token.Utf8Bytes()); //todo pass bot token here from config
+            using var hmacsha256 = new HMACSHA256(secretKey);
+            var dataCheckStringHash = hmacsha256
+                .ComputeHash(query.Key.Utf8Bytes());
+            return new StringBuilder(dataCheckStringHash.Length *
+                                     2) // as actually unit length will increase by two
+                .SideEffect(builder => dataCheckStringHash
+                    .ForEach(b => builder.AppendFormat("{0:x2}", b))
+                )
+                .ToString()
+                .Equals(query.Hash)
+                .Map(Task.FromResult);
+        }
     }
 }
